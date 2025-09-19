b3b37af48f3239bad83a89cdda65698c6f0968f1
Projjwal Raj <piyushraj707@gmail.com>
2025-09-19 12:05:22 +0530

    working correctly

--- MyClient2.java
+++ MyClient2.java
@@ -1,32 +1,43 @@
 import java.security.Security;
 import java.security.SecureRandom;
-import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Properties;
 
-//import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
 import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
-import org.bouncycastle.util.Strings;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class MyClient2 {
-	public static  void main(String[] args) throws Exception {
-		Properties props = System.getProperties();
-		props.setProperty("jdk.tls.namedGroups", "X25519MLKEM768");
-		String HOST_NAME = "www.instagram.com";
+	public static void main(String[] args) throws Exception {
+		System.setProperty("jdk.tls.namedGroups", "X25519MLKEM768");
+
+		String HOST_NAME = "www.cloudflare.com";
 		int PORT = 443;
+
 		Security.addProvider(new BouncyCastleJsseProvider());
+		Security.addProvider(new BouncyCastleProvider());
+
+		TrustManager[] trustAllCerts = new TrustManager[] {
+			new X509TrustManager() {
+				public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+				public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+				public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+			}
+		};
+
 		SSLContext sslContext = SSLContext.getInstance("TLSv1.3", "BCJSSE");
-		TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance("PKIX", "BCJSSE");
-		trustManagerFactory.init((KeyStore) null);
-		sslContext.init(null, trustManagerFactory.getTrustManagers(), new SecureRandom());
+		sslContext.init(null, trustAllCerts, SecureRandom.getInstance("DEFAULT", BouncyCastleProvider.PROVIDER_NAME));
+
 		SSLSocketFactory socketFactory = sslContext.getSocketFactory();
 		SSLSocket cSock = (SSLSocket) socketFactory.createSocket(HOST_NAME, PORT);
 		cSock.startHandshake();
